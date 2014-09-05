a0af2fa3f3fe489a1d54c673a9e4c25df143f854
shaileshgupta2 <itsstsmacmini@ITSSTS-MacMinis-Mac-mini.local>
2014-09-05 17:44:34 +0530

    added a json parser class

---
+++ ProjectTrax/jsonParser.h
@@ -0,0 +1,13 @@
+//
+//  jsonParser.h
+//  ProjectTrax
+//
+//  Created by ITSSTS MacMini on 05/09/14.
+//  Copyright (c) 2014 ITSS TS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jsonParser : NSObject
+
+@end
---
+++ ProjectTrax/jsonParser.m
@@ -0,0 +1,13 @@
+//
+//  jsonParser.m
+//  ProjectTrax
+//
+//  Created by ITSSTS MacMini on 05/09/14.
+//  Copyright (c) 2014 ITSS TS. All rights reserved.
+//
+
+#import "jsonParser.h"
+
+@implementation jsonParser
+
+@end
